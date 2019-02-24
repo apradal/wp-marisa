43e774dbd04422b39cd81fe317a1d2756d6d4b24
apradal <antoniopradalara@gmail.com>
2019-02-24 13:12:25 +0100

    adapting examples of style to real ones.

---
+++ wp-content/themes/onepress-child/template-sobremi.php
@@ -0,0 +1,190 @@
+<?php
+/**
+ * Template Name: Sobremi
+ */
+
+get_header(); ?>
+
+<div id="content" class="custom-template">
+
+    <div id="image-title" class="sobre-mi">
+        <h1 class="title-fade slide-in-top">SOBRE MI</h1>
+    </div>
+
+    <div class="formacion-container container">
+        <div class="row container">
+            <div class="col-12">
+                <h2>FORMACIÓN</h2>
+            </div>
+        </div>
+        <div class="row block container">
+            <div class="col-xs-12 col-md-12 col-lg-6">
+                <p>Licenciada en Psicología por la U.N.E.D., habiendo cursado dentro de la carrera la rama de clínica. Inscrita en Colegio Profesional de Psicólogos de Aragón con el número de Colegiado A-01857.</p>
+                <p>Máster en Prevención y Tratamiento de drogodependencia y otras adicciones.</p>
+                <p>Profesora de Psicofarmacología y Psicopatología.</p>
+                <p>Amplia experiencia en Trastornos de la Personalidad y Trastornos de Ansiedad.</p>
+                <p>Miembro de la Sociedad Española de Patología Dual. (SEPD).</p>
+                <p>Miembro de la Sociedad Aragonesa de Psiquiatría Legal y Ciencias Forenses.</p>
+                <p>Máster en Psicología Forense.</p>
+            </div>
+            <div class="col-xs-12 col-md-12 col-lg-6">
+                <img class="formacion-img" src="http://s681648251.mialojamiento.es/wp-content/uploads/2017/03/psicologa.png" alt=""/>
+            </div>
+        </div>
+    </div>
+
+    <div class="cursos-container">
+        <div class="row block container">
+            <div class="hidden-md-down col-lg-6">
+                <img class="cursos-banner" src="http://wp-marisa.local/wp-content/uploads/2019/02/marisa_banner_3.png" alt=""/>
+            </div>
+            <div class="col-xs-12 col-md-12 col-lg-6 info">
+                <h2>CURSOS REALIZADOS</h2>
+                <p>Conocimiento y Actuación en los Trastornos de la Conducta Alimentaria.</p>
+                <p>Curso de Prevención de Drogodependencia en Adolescentes.</p>
+                <p>Trastornos de la Personalidad por Relaciones de Dependencia.</p>
+                <p>Intervención Psicológica en Situaciones de Crisis y Emergencias.</p>
+                <p>Curso de primeros auxilios y socorrismo.</p>
+                <p>Curso de formación teórico practica para psicólogos del Equipo de Respuesta Inmediata en emergencias de Cruz Roja. ERIE.</p>
+                <p>Curso de acercamiento a la terapia familiar desde una perspectiva sistémica.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="colaboraciones-container container">
+        <div class="block container">
+            <div class="col-xs-12">
+                <h2>Mis Colaboraciones</h2>
+                <p>Colaboraciones a destacar a lo largo de mi carrera profesional como psicóloga.</p>
+            </div>
+        </div>
+
+        <div class="row block container boxes-container">
+            <div class="col-xs-12 col-md-6 col-lg-4 flex">
+                <div class="colaboraciones-box">
+                    <a href="#">
+                        <img src="http://wp-marisa.local/wp-content/uploads/2019/02/jose-carlos.jpg" alt=""/>
+                        <div class="colaboraciones-info">
+                            <h3>DR. D. JOSÉ CARLOS FUERTES ROCAÑÍN</h3>
+                            <p>Psiquiatra Consultor</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-md-6 col-lg-4 flex">
+                <div class="colaboraciones-box">
+                    <a href="#">
+                        <img src="http://wp-marisa.local/wp-content/uploads/2019/02/centro_CITA.png" alt=""/>
+                        <div class="colaboraciones-info">
+                            <h3>CITA CLÍNICA</h3>
+                            <p>Centro de Investigación y tratamiento de adicciones. – Como psicóloga para tratamiento ambulatorio en la provincia de Zaragoza.</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-md-6 col-lg-4 flex">
+                <div class="colaboraciones-box">
+                    <a href="#">
+                        <img src="http://wp-marisa.local/wp-content/uploads/2019/02/dental.png" alt=""/>
+                        <div class="colaboraciones-info">
+                            <h3>CLINICA DENTAL DRA. CHAMORRO PONS</h3>
+                            <p>Colaborando para el tratamiento de la fobia al dentista y a las agujas.</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-md-6 col-lg-4 flex">
+                <div class="colaboraciones-box">
+                    <a href="#">
+                        <img src="http://wp-marisa.local/wp-content/uploads/2019/02/campus2.jpg" alt=""/>
+                        <div class="colaboraciones-info">
+                            <h3>CAMPUS PSICOLOGÍA</h3>
+                            <p>Profesora de Psicofarmacología y Psicopatología.</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-md-6 col-lg-4 flex">
+                <div class="colaboraciones-box">
+                    <a href="#">
+                        <img src="http://wp-marisa.local/wp-content/uploads/2019/02/cruzroja.jpg" alt=""/>
+                        <div class="colaboraciones-info">
+                            <h3>CRUZ ROJA</h3>
+                            <p>Colaborando como psicóloga y educadora de la salud, en la prevención de drogodependencia y trastornos de la conducta alimentaria en adolescentes. Impartiendo cursos de formación al voluntariado. Realizando talleres de sexualidad con adolescentes.</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-md-6 col-lg-4 flex">
+                <div class="colaboraciones-box">
+                    <a href="#">
+                        <img src="http://wp-marisa.local/wp-content/uploads/2018/12/colaboraciones_pyschologies.jpg" alt=""/>
+                        <div class="colaboraciones-info">
+                            <h3>REVISTA “PSYCHOLOGIES”</h3>
+                            <p>Colaborando en diferentes artículos e informando a dicha revista sobre diferentes temas.</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-md-6 col-lg-4 flex">
+                <div class="colaboraciones-box">
+                    <a href="#">
+                        <img src="http://wp-marisa.local/wp-content/uploads/2019/02/logoUNED.jpg" alt=""/>
+                        <div class="colaboraciones-info">
+                            <h3>UNED</h3>
+                            <p>Facultad de Psicología. Como colaboradora y tutora, formando y orientando a los alumnos durante su prácticum de psicología.</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-md-6 col-lg-4 flex">
+                <div class="colaboraciones-box">
+                    <a href="#">
+                        <img src="http://wp-marisa.local/wp-content/uploads/2019/02/aragon-tv.jpg" alt=""/>
+                        <div class="colaboraciones-info">
+                            <h3>ARAGÓN TELEVISIÓN</h3>
+                            <p>Como contertulia en el programa magazine “Sin ir más lejos”.</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-md-6 col-lg-4 flex">
+                <div class="colaboraciones-box">
+                    <a href="#">
+                        <img src="http://wp-marisa.local/wp-content/uploads/2019/02/rosa_blanca.png" alt=""/>
+                        <div class="colaboraciones-info">
+                            <h3>ROSA BLANCA ANDRÉS</h3>
+                            <p>Licenciada en Psicología y Máster en Neuropsicología.</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-container-contact">
+        <div class="container">
+            <div class="bg-img-contact">
+                <img src="http://wp-marisa.local/wp-content/uploads/2019/02/marisa_bg_small.png" alt=""/>
+            </div>
+            <div class="bg-info-contact">
+                <h2 class="alternative-tittle">"Su problema tiene tratamiento"</h2>
+                <h3>RESERVAR CITA </h3>
+                <p><i class="fa fa-phone"></i> 876 712 268</p>
+                <a href="http://wp-marisa.local/contacto">CONTACTO</a>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+<?php get_footer(); ?>
