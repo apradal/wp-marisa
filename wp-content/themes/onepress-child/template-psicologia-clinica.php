3d200917f62ae19ef0d05d7b5296bb073aebb04e
apradal <antoniopradalara@gmail.com>
2019-03-05 12:23:43 +0100

    adding clinica page and style

---
+++ wp-content/themes/onepress-child/template-psicologia-clinica.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Template Name: Psicologia-clinica
+ */
+
+get_header(); ?>
+
+<div id="content" class="custom-template">
+
+    <div id="image-title" class="psicologia">
+        <h1 class="title-fade slide-in-top">PSICOLOGÍA</h1>
+    </div>
+
+    <div class="container">
+        <div class="row block">
+            <div class="col-xs-12 col-lg-8">
+                <p>El tipo de terapia que principalmente se sigue en mi consulta de Psicología en Zaragoza, es la Terapia Cognitivo-Conductual, en la cual además de intervenir sobre el comportamiento inadecuado, también se interviene en las creencias y pensamientos disfuncionales que en la mayoría de las ocasiones nos llevan muchas veces a esas conductas inadecuadas. Generalmente, al principio suele ser necesario 1 sesión por semana teniendo ésta una duración mínima de 1 hora. Posteriormente, según las necesidades de cada paciente, se suelen ir espaciando de forma progresiva en el tiempo, haciendo posible que el paciente ponga en práctica él mismo, su nueva forma de funcionar. Por último, se hace una sesión al mes como seguimiento, durante dos o tres meses para asegurar el correcto funcionamiento del paciente y prevenir futuras recaídas.</p>
+                <p>Otros tipos de intervención: <strong>Terapia Orientada a los Valores, Terapia Dialéctica Conductual, Mindfulness…</strong></p>
+            </div>
+            <div class="col-xs-12 col-lg-4">
+                <img class="full-width-img" src="http://wp-marisa.local/wp-content/uploads/2019/02/test_piscologia-300x200.jpg" alt=""/>
+            </div>
+        </div>
+    </div>
+
+    <div class="row block">
+        <div class="col-12">
+            <div class="full-width-bg alternative-bg">
+                <div class="container">
+                    <h3>¿QUÉ HACE UN PSICÓLOGO?</h3>
+                    <p style="text-align: justify;"><span style="font-family: verdana,geneva,sans-serif; font-size: 10pt;">Cuando una persona sufre un trastorno psicológico, se ven afectadas todas las áreas de su vida, pues nuestros pensamientos influyen en nuestras emociones y a su vez dichas emociones en nuestra conducta. El trabajo del psicólogo consiste en promover cambios a nivel psicológico, de tal forma que se produzcan cambios a nivel cognitivo, influyendo así en las emociones y finalmente se pueda cambiar la conducta, que interfiere de forma negativa en la vida del paciente. Para ello en consulta la terapia que se sigue es de tipo cognitivo-conductual.</span></p>
+                    <p style="text-align: justify;"><span style="font-family: verdana,geneva,sans-serif; font-size: 10pt;">Lo primero que debe hacer el psicólogo es realizar una evaluación psicológica, para poder conocer el caso, y poder aplicar el tratamiento más adecuado durante el menor tiempo posible, ya que una terapia demasiado larga es agotadora y desmotivante para el paciente. Sin olvidar, que en algunos casos, puede ser necesario intervenir en algunos aspectos ya desde la primera sesión, a la vez que se realiza dicha evaluación. Una vez que se tienen claros los objetivos a conseguir, tanto por el psicólogo como por el paciente, se comienza con el aprendizaje de herramientas psicológicas y conductuales, de tal forma que le permitan al paciente superar su problema y las consecuencias derivadas del mismo. A lo largo de la terapia se incluyen evaluaciones del funcionamiento de la misma, que permiten al psicólogo observar si los resultados obtenidos son los deseados</span></p>
+                    <p style="text-align: justify;"><span style="font-family: verdana,geneva,sans-serif; font-size: 10pt;">Finalizada la terapia suelen incluirse sesiones de seguimiento de frecuencia mensual, trimestral o anual, según las necesidades del paciente, que permiten obtener información sobre el caso y comprobar que no se dan recaídas a largo plazo. Su utilidad es comprobar la efectividad del tratamiento y el buen funcionamiento del paciente en su día a día, además de poder reforzar los aprendizajes recibidos en la terapia si fuera necesario.</span></p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="psicologia-clinica">
+            <div class="container">
+                <div class="row">
+                    <div class="hidden-md-down col-lg-6">
+                        misco
+                    </div>
+                    <div class="col-xs-12 col-lg-6">
+                        <h3>PSICOLOGÍA CLÍNICA</h3>
+                        <p>Como <b>psicóloga con especialidad en clínica</b>, cuento con una amplia experiencia en el campo de la psicología clínica y la salud mental, atendiendo en mi consulta de Zaragoza tanto a pacientes con problemas o trastornos psicológicos como pacientes con patologías psiquiátricas.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row block">
+            <div class="col-xs-12 col-md-12">
+                <h3>PATOLOGÍAS DE PSIQUIATRÍA CLÍNICA</h3>
+            </div>
+            <div class="col-xs-6 col-md-6">
+                <h4>TRASTORNOS DE ANSIEDAD</h4>
+                <ul>
+                    <li>Fobias</li>
+                    <li>Crisis de angustia</li>
+                    <li>Trastornos de ansiedad</li>
+                </ul>
+            </div>
+            <div class="col-xs-6 col-md-6">
+                <h4>TRASTORNOS DE AFECTIVOS</h4>
+                <ul>
+                    <li>Depresión</li>
+                    <li>Trastornos Adaptativos</li>
+                </ul>
+            </div>
+            <div class="col-xs-6 col-md-6">
+                <h4>TRASTORNOS DE LA PERSONALIDAD</h4>
+                <ul>
+                    <li>Trastornos de personalidad</li>
+                    <li>Rasgos disfuncionales (Trastorno Obsesivos, dependencia emocional…)</li>
+                </ul>
+            </div>
+            <div class="col-xs-6 col-md-6">
+                <h4>OTRO APOYO EN PATOLOGÍAS DE TIPO PSIQUIÁTRICO</h4>
+                <ul>
+                    <li>Esquizofrenia</li>
+                    <li>Trastorno esquizoafectivo</li>
+                    <li>Trastorno Bipolar</li>
+                    <li>Ideas delirantes</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+<?php get_footer(); ?>
