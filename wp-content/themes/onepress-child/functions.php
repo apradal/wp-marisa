2937dacf38e8f95e65c30548754d39e24687d283
apradal <antoniopradalara@gmail.com>
2019-03-22 16:24:30 +0100

    adding fonts

--- wp-content/themes/onepress-child/functions.php
+++ wp-content/themes/onepress-child/functions.php
@@ -12,4 +12,10 @@ function onepress_child_enqueue_styles() {
     wp_enqueue_script( 'slick-js', get_stylesheet_directory_uri() . '/assets/js/slick.min.js', array(), false, false );
 }
 
+function add_google_fonts() {
+    wp_enqueue_style( 'add_google_fonts', 'https://fonts.googleapis.com/css?family=Nunito|Pacifico', false);
+}
+add_action( 'wp_enqueue_scripts', 'add_google_fonts' );
+https://fonts.googleapis.com/css?family=Pacifico
+
 remove_filter( 'the_content', 'wpautop' );
